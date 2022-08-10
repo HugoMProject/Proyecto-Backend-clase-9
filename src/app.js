6fc99aab1239eb8982346f269db8ee32b9015965
HugoMProject <hugomoyano1996@gmail.com>
2022-08-10 20:59:59 -0300

    se han creado la rutas

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(require('./routes/product'))
 
 
 
-app.listen((''));
+app.listen(3000, ()=> console.log('Servidor iniciado en puerto 3000'));
